579ec6331bdaf14789573ca0570c11f9bce76d49
Arman Hasan <h.arman88@yahoo.com>
2016-09-13 18:43:55 -0400

    motors[].setPower + changed scope of DcMotor declarations from local to global

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop6322.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop6322.java
@@ -12,20 +12,33 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 public class Teleop6322 extends OpMode {
 
+    DcMotor FrontLeft;
+    DcMotor FrontRight;
+    DcMotor BackLeft;
+    DcMotor BackRight;
+
+    //DcMotor[] motor = {FrontRight, FrontLeft, BackRight, BackLeft};
+
+    float lefty1 = gamepad1.left_stick_y;
+    float righty1 = gamepad1.right_stick_y;
+
     @Override
     public void init(){
 
-        DcMotor FrontRight = hardwareMap.dcMotor.get("fr");
-        DcMotor FrontLeft = hardwareMap.dcMotor.get("fl");
-        DcMotor BackRight = hardwareMap.dcMotor.get("br");
-        DcMotor BackLeft = hardwareMap.dcMotor.get("bl");
-
-        DcMotor[] motor = {FrontRight, FrontLeft, BackRight, BackLeft};
+        FrontRight = hardwareMap.dcMotor.get("fr");
+        FrontLeft = hardwareMap.dcMotor.get("fl");
+        BackRight = hardwareMap.dcMotor.get("br");
+        BackLeft = hardwareMap.dcMotor.get("bl");
 
     }
 
     @Override
     public void loop(){
+
+        FrontLeft.setPower(lefty1);
+        FrontRight.setPower(righty1);
+        BackLeft.setPower(lefty1);
+        BackRight.setPower(righty1);
 
     }
 }
