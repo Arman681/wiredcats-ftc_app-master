f949bd4a531ff40ae2a2ef0312625605b2a5ea63
SethHorwitz <horwitzsethedu@gmail.com>
2016-11-19 08:06:27 -0500

    Updating red auto for 10366

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedAuto10366.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+/**
+ * Created by SethHorwitz on 11/18/16.
+ */
+@Autonomous(name="RedAuto10366", group="Autonomous")
+public class RedAuto10366 {
+
+
+}
