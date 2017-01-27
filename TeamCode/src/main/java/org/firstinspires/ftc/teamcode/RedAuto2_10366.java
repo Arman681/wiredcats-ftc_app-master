cb531311484d2528496ad4856bf1319a25151f64
Carol <c_saponaro@hotmail.com>
2017-01-27 14:55:02 -0500

    Changed  comments and shooting

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedAuto2_10366.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedAuto2_10366.java
@@ -164,7 +164,7 @@ public class RedAuto2_10366 extends LinearOpMode {
 
         moveByTime(-0.25, 1000); //move Backward at one-quarter speed for 1.250 seconds*** changed to 1000***
 
-        // Shoot 2 Particles
+        //Shoot 2 Particles
 
         shoot(1.0, 5.5, 1.75); //Shoots particles at full power for  change  changed to 4.75 / 1.5
 
@@ -172,19 +172,22 @@ public class RedAuto2_10366 extends LinearOpMode {
 
 
 
-        //Claim Blue Cap Ball
+        //Claim Red Cap Ball
 
         moveByTime(-0.25, 500); //Move Backwards at one-quarter speed for .5 second
         turnByTime(0.25, ninetydegrees); //Turns Clock-wise at half speed for X seconds to make90-DEGREE turn (2 X 45-DEGREE) ***Should push cap ball***
+        moveByTime(0.25, 750); //Move Forwards  at one-quarter speed for 1.5 seconds changed to .5 seconds for longer distance
         moveByTime(0.25, 500); //Move Forwards  at one-quarter speed for 1.5 seconds changed to .5 seconds for longer distance
         // turnByTime(0.25, fourtyfivedegrees); //Turns Clock-wise at one-quarter speed for .4 to make 45-DEGREE turn
         // moveByTime(0.25, 1500); //Move forwards at one-quarter speed for 1.5 seconds
 
-        //Park at Blue Corner Vortex
+        //Park at Red Corner Vortex
 
         turnByTime(-0.25, fourtyfivedegrees); //Turns Counter Clock-wise at one-quarter speed for three-quarters of a second to make 45-degree turn
-        moveByTime(0.25, 500); //Move  Forwards at one-quarter speed for one-half second
-        moveByTime(0.25, 1000); //Move Forwards at one-quarter speed for one second
+        moveByTime(0.25, 750); //Move  Forwards at one-quarter speed for one-half second
+        moveByTime(0.25, 750); //Move Forwards at one-quarter speed for one second
+        moveByTime(0.25, 750); //Move Forwards at one-quarter speed for one second
+        moveByTime(0.25, 500); //Move Forwards at one-quarter speed for one second
         stopAllMotors();
     }
 
@@ -226,7 +229,7 @@ public class RedAuto2_10366 extends LinearOpMode {
     }
 
 
-    //Determine Blue Side
+    //Determine Red Side
 
     public String determineBlueSide() throws InterruptedException {
 
