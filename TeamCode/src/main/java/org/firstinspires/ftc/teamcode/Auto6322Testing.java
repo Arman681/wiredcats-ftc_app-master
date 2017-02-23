608226c5af5e77acacba16a3d91c049fc38aae67
SethHorwitz <horwitzsethedu@gmail.com>
2017-02-23 14:42:25 -0500

    Testing a new autonomous program.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto6322Testing.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto6322Testing.java
@@ -243,7 +243,7 @@ public class Auto6322Testing extends Auto6322Red{
         moveByTime(0.0, 500);
 
 
-        driveStraight(0.5, 27);
+        driveStraight(0.5, 30);
         moveByTime(0.0, 500);
 
         turnBySteps(0.5, -2.2);
